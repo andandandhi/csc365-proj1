f160701ba897fcfb1283cb192dcd65287fa0769f
andandandhi <andrewducdanguniv@gmail.com>
2023-08-23 14:16:22 -0700

    calling it for wednesay aug 23

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.restaurant {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
             
                             
     opens com.example.restaurant to javafx.fxml;
