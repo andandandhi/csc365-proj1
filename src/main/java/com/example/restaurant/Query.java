800f1c1673b4e05afc97759c2d2dd1043e1fb250
andandandhi <andrewducdanguniv@gmail.com>
2023-08-24 12:32:20 -0700

    add query class with all dish query

---
+++ src/main/java/com/example/restaurant/Query.java
@@ -0,0 +1,62 @@
+package com.example.restaurant;
+
+import java.sql.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Query{
+
+        static   Connection connect;
+
+        public List<String> getDishNames(){
+            List<String> dishNamesList = new ArrayList<String>();
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connect = DriverManager.getConnection(
+                        "jdbc:mysql://ambari-node5.csc.calpoly.edu:3306/restaurant?user=restaurant&password=csc365");
+                Statement statement = connect.createStatement();
+                ResultSet rs = statement.executeQuery(
+                        "SELECT * FROM Dishes");
+                while (rs.next()) {
+                    String dName = rs.getString(2);
+                    dishNamesList.add(dName);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return dishNamesList;
+        }
+
+    public List<Dish> getDishes(){
+        List<Dish> dishList = new ArrayList<Dish>();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection(
+                    "jdbc:mysql://ambari-node5.csc.calpoly.edu:3306/restaurant?user=restaurant&password=csc365");
+            Statement statement = connect.createStatement();
+            ResultSet rs = statement.executeQuery(
+                    "SELECT * FROM Dishes");
+            while (rs.next()) {
+                int did = rs.getInt(1);
+                String dName = rs.getString(2);
+                String description = rs.getString(3);
+                double price = rs.getDouble(4);
+                Dish d = new Dish(did, dName, description, price);
+                dishList.add(d);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dishList;
+    }
+
+        public static void main(String[] args) {
+            Query q = new Query();
+            List<String> dn = q.getDishNames();
+            for(int i = 0; i < dn.size(); i++){
+                System.out.println("Dish Name = " + dn.get(i));
+            }
+        }
+
+}
