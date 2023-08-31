4e13813ee278a6dff8773aa323c74f7b1507e763
tkonchok <136930789+tkonchok@users.noreply.github.com>
2023-08-31 16:43:07 -0700

    Create LedgerApplication.java

---
+++ LedgerApplication.java
@@ -0,0 +1,28 @@
+package com.example.restaurantledger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LedgerApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("ledger-skeleton.fxml"));
+
+        Scene scene = new Scene(root, 530, 611);
+
+        primaryStage.setTitle("Ledger Application");
+
+        primaryStage.setScene(scene);
+        
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
